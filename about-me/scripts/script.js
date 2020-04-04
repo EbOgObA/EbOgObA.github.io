16c7917c3fc8c81fd738ef4c030b0e2d3ab03c31
EbOgObA <dmitry_musaev@mail.ru>
2020-04-04 18:06:09 +0300

    share button

--- about-me/scripts/script.js
+++ about-me/scripts/script.js
@@ -158,4 +158,27 @@ $("form").submit(function() { //Change
         }
       ]
     });
+
+
+
+    // Кнопка "Поделиться"
+    $('.share-button').on('click',function(){
+      $(this).addClass('open');
+    });
+    $(".share-items").draggable({
+      axis: "x",
+      containment : ".share-items-wrapper"
+    });
+    $(".share-item").on('click',function(e){
+      e.preventDefault();
+      $('.share-button').addClass('shared');
+      setTimeout(function(){
+        $('.share-button').addClass('thankyou');
+      }, 800);
+      setTimeout(function(){
+        $('.share-button').removeClass('open');
+        $('.share-button').removeClass('shared');
+        $('.share-button').removeClass('thankyou');
+      }, 2500);
+    });
 });
