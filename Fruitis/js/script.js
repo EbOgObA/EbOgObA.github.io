a5d3e361e0cc4ba1812c2dbe952c937d3bb3e334
EbOgObA <dmitry_musaev@mail.ru>
2020-04-27 22:11:02 +0300

    about us

--- Fruitis/js/script.js
+++ Fruitis/js/script.js
@@ -6,3 +6,11 @@ $(".menu-burger").on('click',function(){
   $(this).find(".hambergerIcon").toggleClass("open");
   $('.header-nav').slideToggle();
 });
+
+// Anchors
+$(".nav-item__anchors").on("click", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top - 80;
+    $('body,html').animate({scrollTop: top}, 1500);
+});
