293478d01fa9395b5964d4f074e474b57e8e9890
EbOgObA <dmitry_musaev@mail.ru>
2020-06-21 23:58:53 +0300

    hover effects

--- AOD/js/script.js
+++ AOD/js/script.js
@@ -63,6 +63,20 @@ $(document).ready(function(){
         });
     }
 
+    $('.item')
+    .on('mouseenter', function(e) {
+        let parentOffset = $(this).offset(),
+            relX = e.pageX - parentOffset.left,
+            relY = e.pageY - parentOffset.top;
+        $(this).find('.item__overlay').css({top:relY, left:relX})
+    })
+    .on('mouseout', function(e) {
+        let parentOffset = $(this).offset(),
+            relX = e.pageX - parentOffset.left,
+            relY = e.pageY - parentOffset.top;
+        $(this).find('.item__overlay').css({top:relY, left:relX})
+    });
+
     //FORMS
     function forms(){
         //SELECT
