9b671f0b6b4ef464ba8c69f1ce8db036ba478ab9
EbOgObA <62988274+EbOgObA@users.noreply.github.com>
2020-09-03 00:14:14 +0300

    fix bag check terms of use

--- 16bit/js/script.js
+++ 16bit/js/script.js
@@ -203,21 +203,97 @@ $(document).ready(function () {
     // });
 
     // =======================================================================================================================
+    
 
-    $('.popup-details__btn-continue').on('click', function(e){
-        if ($('.check-terms').hasClass('active')) {
-            return true;
-            // sessionStorage.setItem('check', 'true');
-        } else {
-            $('.check-lic').addClass('err');
-            // sessionStorage.removeItem('check', 'true');
-            e.preventDefault();
+    // CHECK TERMS OF USE
+
+    // простенький класс, для упрощения сохранения 
+    // и чтения массивов объектов в localStorage
+    class Stogage {
+        constructor(name){
+            // конструктор. создает в localStorage 
+            // именованное хранилище
+            // запоминаем название именованного хранилища
+            this.name = name;
+            // создаем ассоциативный массив в котором для
+            // быстрого доступа будут кэшироваться объекты
+            // сохраняемые в именованном хранилище
+            this.hash = {};
+            // если в localStorage уже есть данные 
+            // сохраненые под ключем с именем this.name
+            // то считываем их в this.hash
+            let text = localStorage.getItem(this.name);
+            if(text)
+                this.hash = JSON.parse(text);
+                // сохраняем ассоциативный массив this.hash в
+                // localStorage под именем this.name 
+                this.save();  
         }
-    });
-    $('.popup-details__info label').on('click', function(e){
-            $('.check-lic').removeClass('err');
-    });
+        get(id){
+            // получить значение по его id из именованного хранилища 
+            // с именем this.name
+            return this.item.find(item=>item.id===id)
+        }
+        add(id, data){
+            // сохранить значение по его id в именованном хранилище
+            // с именем this.name
+            this.hash[id] = data;
+            this.save();
+        }
+        del(id){
+            // удалить значение по его id в именованном хранилище
+            // с именем this.name
+            delete this.hash[id];
+            this.save();
+        }
+        save(){
+            // преобразуем ассоциативный массив this.hash в массив list
+            this.list = Object.values(this.hash);
+            // преобразуем массив list в строку text
+            const text = JSON.stringify(this.hash);
+            // сохраняем строку text в localStorage под
+            // именем this.name
+            localStorage.setItem(this.name, text);      
+        }
         
+    }
+    // создаем объект класса Stogage для хранения данных о состоянии checkbox-в в localStorage
+    const checkbox_store = new Stogage('checkbox_store');
+    // восстанавливает состояния checkbox-в сохраненные в localstorage checkbox_store
+    checkbox_store.list.forEach(item=>{
+    // если есть такой элемент, выставляем ему запомненное состояние
+        if( item.state === "on" )
+            return $('#' + item.id).prop('checked', item.state);
+            checkbox_store.del(item.id);
+    });
+    // устанавливаем обработчик для фиксации изменения состояния checkbox-в с классом check
+    $('body').on('change', '.check', function(event){
+        let id = $(this).attr('id');
+        let state = event.currentTarget.checked?"on":undefined;
+        checkbox_store.add(id, {
+            id: id,
+            state: state
+        });
+    });
+
+
+    $('.check-terms__label').on('click', function(e){
+        $('.check-lic').removeClass('err');
+    });
+    $('.popup-details__btn-continue').on('click', function(e){
+        let state;
+        checkbox_store.list.forEach(function(value,key) {
+            return state = value.state;
+        });
+        if (state === undefined || checkbox_store.list.length == 0) {
+            e.preventDefault();
+            $('.check-lic').addClass('err');
+        } else {
+            return true;
+        }
+    });  
+
+
     // =======================================================================================================================
 
     // $('.form-aut__input-button-refresh').on('click', function() {
@@ -626,21 +702,9 @@ $(document).ready(function () {
     
     $('.franchise-var').on('click', function() {
         $(this).parent().addClass('active').siblings().removeClass('active');
-        let valFranch = $(this).parent().find('.item-choose__info-price').text();
+        let valFranch = $(this).next().children('.item-choose__info-price').text();
         $('.franchise-value').text(valFranch)
     });
-    // $('.franchise1').on('click', function(){
-    //     $('.franchise1').addClass('active');    
-    //     $('.value-cal').text("1500");
-    // });
-    // $('.franchise2').on('click', function(){
-    //     $('.franchise2').addClass('active');    
-    //     $('.value-cal').text("1000");
-    // });
-    // $('.franchise3').on('click', function(){  
-    //     $('.franchise3').addClass('active');    
-    //     $('.value-cal').text("500");
-    // });
 
     // =======================================================================================================================
 
