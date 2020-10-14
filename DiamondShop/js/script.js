b0cf2a1c0a3c5afd755927a22741cccf1eb1accf
EbOgObA <62988274+EbOgObA@users.noreply.github.com>
2020-10-14 14:31:28 +0300

    catalog fixed bags

--- DiamondShop/js/script.js
+++ DiamondShop/js/script.js
@@ -166,65 +166,8 @@ $(document).ready(function () {
 
     // =======================================================================================================================
 
-    //FORMS
+    // FORMS
     function forms() {
-        // INPUT
-        $('input,textarea').focus(function () {
-            if ($(this).val() == $(this).attr('data-value')) {
-                $(this).addClass('focus');
-                $(this).parent().addClass('focus');
-                $(this).val('');
-            };
-            removeError($(this));
-        });
-        // RANGE
-        if ($("#range").length > 0) {
-            $("#range").slider({
-                range: true,
-                min: 100,
-                max: 10000,
-                values: [100, 10000],
-                slide: function (event, ui) {
-                    $('#rangefrom').val(ui.values[0]);
-                    $('#rangeto').val(ui.values[1]);
-                    $(this).find('.ui-slider-handle').eq(0).html('<span>' + ui.values[0] + '</span>');
-                    $(this).find('.ui-slider-handle').eq(1).html('<span>' + ui.values[1] + '</span>');
-                },
-                change: function (event, ui) {
-                    if (ui.values[0] != $("#range").slider("option", "min") || ui.values[1] != $("#range").slider("option", "max")) {
-                        $('#range').addClass('act');
-                    } else {
-                        $('#range').removeClass('act');
-                    }
-                }
-            });
-            $('#rangefrom').val($("#range").slider("values", 0));
-            $('#rangeto').val($("#range").slider("values", 1));
-
-            $("#range").find('.ui-slider-handle').eq(0).html('<span>' + $("#range").slider("option", "min") + '</span>');
-            $("#range").find('.ui-slider-handle').eq(1).html('<span>' + $("#range").slider("option", "max") + '</span>');
-
-            $("#rangefrom").bind("change", function () {
-                if ($(this).val() * 1 > $("#range").slider("option", "max") * 1) {
-                    $(this).val($("#range").slider("option", "max"));
-                }
-                if ($(this).val() * 1 < $("#range").slider("option", "min") * 1) {
-                    $(this).val($("#range").slider("option", "min"));
-                }
-                $("#range").slider("values", 0, $(this).val());
-            });
-            $("#rangeto").bind("change", function () {
-                if ($(this).val() * 1 > $("#range").slider("option", "max") * 1) {
-                    $(this).val($("#range").slider("option", "max"));
-                }
-                if ($(this).val() * 1 < $("#range").slider("option", "min") * 1) {
-                    $(this).val($("#range").slider("option", "min"));
-                }
-                $("#range").slider("values", 1, $(this).val());
-            });
-            $("#range").find('.ui-slider-handle').eq(0).addClass('left');
-            $("#range").find('.ui-slider-handle').eq(1).addClass('right');
-        }
         //RATING
         $('.rating.edit .star').hover(function() {
             var block=$(this).parents('.rating');
@@ -256,44 +199,6 @@ $(document).ready(function () {
         }
     }
     forms();
-    //VALIDATE FORMS
-    $('form button[type=submit]').click(function () {
-        var er = 0;
-        var form = $(this).parents('form');
-        var ms = form.data('ms');
-        $.each(form.find('.req'), function (index, val) {
-            er += formValidate($(this));
-        });
-        if (er == 0) {
-            removeFormError(form);
-        } else {
-            return false;
-        }
-    });
-    function formValidate(input) {
-        var er = 0;
-        if (input.attr('name') == 'email' || input.hasClass('email')) {
-            if (input.val() != input.attr('data-value')) {
-                var em = input.val().replace(" ", "");
-                input.val(em);
-            }
-            if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.val())) || input.val() == input.attr('data-value')) {
-                er++;
-                addError(input);
-            } else {
-                removeError(input);
-            }
-        }
-        return er;
-    }
-    function addError(input) {
-        input.addClass('err');
-        input.parent().addClass('err');
-    }
-    function removeError(input) {
-        input.removeClass('err');
-        input.parent().removeClass('err');
-    }
 
     // =======================================================================================================================
 
