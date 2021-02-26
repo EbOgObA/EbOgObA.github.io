5155740de7077c2824455cc3e57c43c7781f407d
EbOgObA <62988274+EbOgObA@users.noreply.github.com>
2021-02-26 04:07:03 +0300

    New PinUp Main Page

---
+++ PinUpNew/js/script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function(){
+	// BURGER
+	let iconMenu = document.querySelector(".icon-menu");
+	let body = document.querySelector("body");
+	let menuBody = document.querySelector(".menu");
+	let menuOverlay = document.querySelector(".menu-overlay");
+	if (iconMenu) {
+		iconMenu.addEventListener("click", function () {
+			iconMenu.classList.toggle("active");
+			menuBody.classList.toggle("active");
+			menuOverlay.classList.toggle("active");
+			body.classList.toggle("lock");
+		});
+	}
+
+	// SLIDER
+	let slider = document.querySelector('.swiper-container');
+	let swiperPartner = new Swiper(slider,{
+		slidesPerView: 1,
+		loop: true,
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+		}
+	});
+
+	// FAVORITE
+	let favorite = document.querySelectorAll('.favorite');
+	favorite.forEach(element => {
+		element.onclick = function() {
+			this.classList.toggle('active');
+		};
+	});
+
+
+});
