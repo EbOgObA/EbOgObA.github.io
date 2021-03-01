05c16daae5b7bb0a48e2272646bd6d48133a4ad3
EbOgObA <62988274+EbOgObA@users.noreply.github.com>
2021-03-01 23:01:35 +0300

    Fixed StarRating - PinUp

--- PinUp/js/script.js
+++ PinUp/js/script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
 	
+
 	// BURGER
 	let iconMenu = document.querySelector(".icon-menu");
 	let menuBody = document.querySelector(".menu");
@@ -19,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function(){
 		body.classList.toggle("lock");
 	});
 
+
 	// SLIDER
 	let slider = document.querySelector('.swiper-container');
 	let swiperPartner = new Swiper(slider,{
@@ -31,6 +33,7 @@ document.addEventListener('DOMContentLoaded', function(){
 			clickable: true,
 		}
 	});
+
 
 	// FAVORITE
 	let favorite = document.querySelectorAll('.favorite');
@@ -117,4 +120,114 @@ document.addEventListener('DOMContentLoaded', function(){
 				Element.prototype.msMatchesSelector;
 		}
 	})();
+
+
+	// RATING
+	const ratings = document.querySelectorAll('.rating');
+	if (ratings.length > 0) {
+		initRatings();
+	}
+
+	// Основная функция
+	function initRatings() {
+		let ratingActive, ratingValue;
+		// "Бегаем" по всем рейтингам на странице
+		for (let index = 0; index < ratings.length; index++) {
+			const rating = ratings[index];
+			initRating(rating);
+		}
+
+		// Инициализируем конкретный рейтинг
+		function initRating(rating) {
+			initRatingVars(rating);
+
+			setRatingActiveWidth();
+
+			if (rating.classList.contains('rating_set')) {
+				setRating(rating);
+			}
+		}
+
+		// Инициализайция переменных
+		function initRatingVars(rating) {
+			ratingActive = rating.querySelector('.rating__active');
+			ratingValue = rating.querySelector('.rating__value');
+		}
+		// Изменяем ширину активных звезд
+		function setRatingActiveWidth(index = ratingValue.innerHTML) {
+			const ratingActiveWidth = index / 0.05;
+			ratingActive.style.width = `${ratingActiveWidth}%`;
+		}
+		// Возможность указать оценку 
+		function setRating(rating) {
+			const ratingItems = rating.querySelectorAll('.rating__item');
+			for (let index = 0; index < ratingItems.length; index++) {
+				const ratingItem = ratingItems[index];
+				ratingItem.addEventListener("mouseenter", function (e) {
+					// Обновление переменных
+					initRatingVars(rating);
+					// Обновление активных звезд
+					setRatingActiveWidth(ratingItem.value);
+				});
+				ratingItem.addEventListener("mouseleave", function (e) {
+					// Обновление активных звезд
+					setRatingActiveWidth();
+				});
+				ratingItem.addEventListener("click", function (e) {
+					// Обновление переменных
+					initRatingVars(rating);
+
+					if (rating.dataset.ajax) {
+						// "Отправить" на сервер
+						setRatingValue(ratingItem.value, rating);
+					} else {
+						// Отобразить указанную оцнку
+						ratingValue.innerHTML = index + 1;
+						setRatingActiveWidth();
+					}
+				});
+			}
+		}
+
+		async function setRatingValue(value, rating) {
+			if (!rating.classList.contains('rating_sending')) {
+				rating.classList.add('rating_sending');
+
+				// Отправика данных (value) на сервер
+				let response = await fetch('rating.json', {
+					method: 'GET',
+
+					//body: JSON.stringify({
+					//	userRating: value
+					//}),
+					//headers: {
+					//	'content-type': 'application/json'
+					//}
+
+				});
+				if (response.ok) {
+					const result = await response.json();
+
+					// Получаем новый рейтинг
+					const newRating = result.newRating;
+
+					// Вывод нового среднего результата
+					ratingValue.innerHTML = newRating;
+
+					// Обновление активных звезд
+					setRatingActiveWidth();
+
+					rating.classList.remove('rating_sending');
+				} else {
+					alert("Ошибка");
+
+					rating.classList.remove('rating_sending');
+				}
+			}
+		}
+	}
+
+
+	
+
 });
