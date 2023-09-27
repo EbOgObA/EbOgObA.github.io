1c812a569c25039273bcd439d59766f4fb9dcb32
ebogoba <dmitry_musaev@mail.ru>
2023-09-27 19:37:32 +0300

    delete wrong Blanchard project

--- Projects/Blanchard/gulp/tasks/server.js
+++
@@ -1,9 +0,0 @@
-export const server = (done) => {
-  app.plugins.browsersync.init({
-    server: {
-      baseDir: `${app.path.build.html}` // Базовая папка, откуда запускать файлы (результат проекта)
-    },
-    notify: false, // Убираются сообщения в браузере
-    port: 3000 // Порт для локального сервера
-  })
-}
